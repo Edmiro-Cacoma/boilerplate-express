88dd93ee9b675d35b43d4efe2361ccee5a51c76d
Edmiro-Cacoma <62661230+Edmiro-Cacoma@users.noreply.github.com>
2024-02-07 08:10:31 +0100

    serving static files

--- myApp.js
+++ myApp.js
@@ -1,39 +1,12 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+const absolutepath = __dirname + "/views/index.html";
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", function (req, res) {
+  res.sendFile(absolutepath);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
